41cadde71fdab694db4e24143c2f791ef90607d8
mzaini30 <muhzaini30@gmail.com>
2017-03-31 09:38:56 +0800

    yey

---
+++ app-windows.py
@@ -0,0 +1,98 @@
+import glob, re, random, string, os
+
+lokasi = ""
+
+indexhtml = open(lokasi+"index.html", "w")
+konten = open(lokasi+"_template/konten.txt").read()
+konten = string.Template(konten)
+index = open(lokasi+"_template/index.txt").read()
+index = string.Template(index)
+
+postingan = glob.glob(lokasi+"post/*.md")
+fpostingan = postingan[:]
+target = postingan[:]
+judul = postingan[:]
+
+print postingan
+
+semuahtml = glob.glob(lokasi+"html/*.html")
+for x in semuahtml:
+    os.remove(x)
+    
+for n, x in enumerate(target):
+    target[n] = re.sub(r"/post/", r"/html/", target[n])
+    target[n] = re.sub(r" ", r"-", target[n])
+    target[n] = target[n][:-3] + ".html"
+link = target[:]
+
+for n, x in enumerate(judul):
+    judul[n] = re.sub(r""+lokasi+"post/", r"", judul[n])
+    judul[n] = re.sub(r".md", r"", judul[n])
+    judul[n] = re.sub(r"\(tanya\)", r"?", judul[n])
+    judul[n] = judul[n].title()
+    
+for n, x in enumerate(judul):
+    print str(n+1)+". ",
+    print x
+    
+for n, x in enumerate(link):
+    link[n] = re.sub(r""+lokasi+"html/", r"", link[n])
+more = []
+
+for n, x in enumerate(judul):
+    more.append(judul[n]+"gebfhahs"+link[n])
+more = [x.split("gebfhahs") for x in more]
+
+situs = "https://ruangintrovert.github.io"
+judulsitus = "Ruang Introvert"
+
+for n, x in enumerate(postingan):
+    random.shuffle(more)
+    title = judul[n]
+    isi = open(x).read()
+    deskripsi = isi[:150]
+    deskripsi = re.sub(r"\n", r" ", deskripsi)
+    deskripsi = re.sub(r"\"", r"'", deskripsi)
+    deskripsi = re.sub(r"\*\*", r"", deskripsi)
+    judul1 = more[0][0]
+    judul2 = more[1][0]
+    judul3 = more[2][0]
+    judul4 = more[3][0]
+    judul5 = more[4][0]
+    link1 = more[0][1]
+    link2 = more[1][1]
+    link3 = more[2][1]
+    link4 = more[3][1]
+    link5 = more[4][1]
+    situslengkap = situs+"/html/"+link[n]
+    dict = {
+    	    "judul": title,
+    	    "isi": isi,
+    	    "deskripsi": deskripsi,
+    	    "judul1": judul1,
+    	    "judul2": judul2,
+    	    "judul3": judul3,
+    	    "judul4": judul4,
+    	    "judul5": judul5,
+    	    "link1": link1,
+    	    "link2": link2,
+    	    "link3": link3,
+    	    "link4": link4,
+    	    "link5": link5,
+    	    "situs": situs,
+    	    "situslengkap": situslengkap,
+    	    "judulsitus": judulsitus
+    	}
+    hasil = konten.substitute(dict)
+    open(target[n], "w").write(hasil)
+    
+banyak = len(link)
+banyak = random.randrange(banyak)
+direct = link[banyak]
+kamus = {
+    "situs": situs,
+    "judulsitus": judulsitus,
+    "direct": direct
+}
+selesai = index.substitute(kamus)
+indexhtml.write(selesai)
---
+++ post/tips agar mental kuat.md
@@ -0,0 +1,21 @@
+**Mas tips agar mental kuat gimana?**
+
+**(Ruby)**
+
+Hai Ruby, boleh tau nggak, menurutmu mental yang kuat itu gimana? Apakah tegar menghadapi setiap rintangan? Apakah mental yang kuat itu adalah kamu terus bisa tersenyum walaupun berbagai ujian datang melanda? Nah, mungkin nasihatku berikut ini bisa membantumu.
+
+Mental itu kan ada dua, mental yang kuat dan yang lemah. Orang yang bermental kuat adalah orang yang siap menghadapi apapun. Mengapa dia siap? Karena dia sebelumnya sudah melakukan persiapan. Dan dia juga sudah melakukan latihan-latihan yang membuatnya tidak kaget dalam menerima hambatan apapun.
+
+Sekarang, gini deh, kan semuanya itu dari otak kita kan ya, Ruby?
+
+Itu hanyalah permasalahan mindset. Kalau kita bisa mengubah mindset kita, maka seakan-akan nggak ada lo yang namanya nggak mungkin. Semuanya menjadi mungkin.
+
+Tapi, apakah kita harus menyerah pada mindset kita? Maksudnya, apakah kita merasa bahwa otak kita ini adalah otak orang yang kalah? Jangan sampai berpikiran begitu deh. Otak itu kan suatu benda yang sangat lunak kan ya? Gampang banget gitu buat bentuk-bentuknya. Istilahnya kan, otak itu adalah suatu materi yang abstrak, fleksibel. Lalu, apa yang benar-benar absolut? Itu adalah sikap dan perilaku kita.
+
+Jadi, ada korelasi antara pikiran kita, perilaku kita, dan kepribadian kita.
+
+Kalau kita bisa melakukan yang menunjukkan kita tegar, maka, otak kita pun akan mengikutinya.
+
+Jadi, jangan berkecil hati ya. Kita semua bisa kok melakukan apa yang kita inginkan.
+
+<script type="text/javascript">nama="Ruby"</script>
